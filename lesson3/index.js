6ecfc4af96296439a3864e7c80e7056c7a39038f
ke4a <helpomos@gmail.com>
2021-08-04 19:39:59 +0500

    lesson3

--- lesson3/index.js
+++ lesson3/index.js
@@ -2,17 +2,10 @@ const fs = require('fs')
 
 const ipArray = ['89.123.1.41', '34.48.240.111']
 
-const readStream = fs.createReadStream('./lesson3/access.log', 'UTF-8')
 const writeStreamIp1 = fs.createWriteStream('./lesson3/89.123.1.41_requests.log', { flags: 'a', encoding: 'utf-8' })
 const writeStreamIp2 = fs.createWriteStream('./lesson3/34.48.240.111_requests.log', { flags: 'a', encoding: 'utf-8' })
 
-readStream.on('end', () => {
-  console.log('Конец чтения файла access.log')
-
-  writeStreamIp1.end(() => console.log('Конец записи 89.123.1.41_requests.log'))
-  writeStreamIp2.end(() => console.log('Конец записи 34.48.240.111_requests.log'))
-})
-readStream.on('error', (err) => console.log(err))
+const readStream = fs.createReadStream('./lesson3/access.log', 'UTF-8')
 
 readStream.on('data', (chunk) => {
   chunk.split('\n')
@@ -24,3 +17,11 @@ readStream.on('data', (chunk) => {
       }
     })
 })
+
+readStream.on('end', () => {
+  console.log('Конец чтения файла access.log')
+
+  writeStreamIp1.end(() => console.log('Конец записи 89.123.1.41_requests.log'))
+  writeStreamIp2.end(() => console.log('Конец записи 34.48.240.111_requests.log'))
+})
+readStream.on('error', (err) => console.log(err))
