ff99497b3b6e0b8ce664e2de5cfa58b20519b9fd
ke4a <helpomos@gmail.com>
2021-08-04 12:19:35 +0500

    lesson2 fix

--- lesson3/index.js
+++ lesson3/index.js
@@ -1,16 +1,22 @@
 const events = require('events')
 
 const startTimer = (date, name) => {
-  const idIntervals = setInterval(() => console.log(`[${name}] : ${((date - Date.now()) / 1000).toFixed()} секунд`), 1000)
+  const idIntervals = setInterval(() => {
+    const time = ((date - Date.now()) / 1000).toFixed()
+    console.log(`[${name}] ${Math.floor(time / (60 * 60))} : ${Math.floor((time / 60) % 60)} : ${Math.floor(time % 60)}`)
+  }, 1000)
+
   setTimeout(() => {
-    ee.emit('end_timer', name)
-    clearInterval(idIntervals)
+    ee.emit('end_timer', name, idIntervals)
   }, date - Date.now())
 }
 
 const ee = new events.EventEmitter()
+
 ee.on('start_timer', startTimer)
-ee.on('end_timer', (name) => console.log(`Таймер ${name} закончился `))
+ee.on('end_timer', (name, idIntervals) => {
+  clearInterval(idIntervals)
+})
 
 const setTimer = (arg) => {
   let count = 0
