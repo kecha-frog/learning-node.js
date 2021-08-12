80bcf94d593756ee361ec836bdd8b535f6a18fee
ke4a <helpomos@gmail.com>
2021-08-12 21:07:00 +0500

    lesson5 test

--- lesson5/server.js
+++ lesson5/server.js
@@ -4,6 +4,8 @@ const fs = require('fs')
 const http = require('http')
 const url = require('url')
 
+let currentDirection = __dirname
+
 const createLink = (pathDir, value) => {
   if (!fs.lstatSync(path.join(pathDir, value)).isFile()) {
     return `<a href=${'http://localhost:3000/?type=dir&name=' + value}>[Dir] ${value}</a><br>`
@@ -13,20 +15,30 @@ const createLink = (pathDir, value) => {
 }
 
 const server = http.createServer((req, res) => {
-  const currentDirection = 'C:\\Users\\lllll\\WebstormProjects\\gb_node'
-  const dirLink = fs.readdirSync(currentDirection).reverse()
   const { type, name } = url.parse(req.url, true).query
-  console.log(type, name)
 
   if (type === 'file') {
     const readStream = fs.createReadStream(path.join(currentDirection, name), { encoding: 'utf8' })
+
     readStream.on('data', chunk => res.write(chunk))
     readStream.on('end', () => res.end())
   } else {
+    if (name) {
+      currentDirection = path.join(currentDirection, name)
+    }
+
+    if (type === 'lastDir') {
+      currentDirection = currentDirection.substring(0, currentDirection.lastIndexOf('\\'))
+    }
+
+    const dirLink = fs.readdirSync(currentDirection).reverse()
+
     res.writeHead(200, 'ok', {
       'Content-Type': 'text/html'
     })
 
+    res.write(`current direction: ${currentDirection}<br>`)
+    res.write('<a href=\'http://localhost:3000/?type=lastDir\'>(...)</a><br>')
     dirLink.map(value => res.write(createLink(currentDirection, value)))
     res.end()
   }
