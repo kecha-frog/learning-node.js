8417f0a91953dd5c5b9c89da97e236faf351f9e2
ke4a <helpomos@gmail.com>
2021-08-13 15:10:28 +0500

    lesson5

--- lesson5/server.js
+++ lesson5/server.js
@@ -1,13 +1,16 @@
-#!/usr/bin/env node
 const path = require('path')
 const fs = require('fs')
 const http = require('http')
 const url = require('url')
 
-let currentDirection = __dirname
+let currentDirection = process.cwd()
+
+const isFile = (pathDir, value) => {
+  return fs.lstatSync(path.join(pathDir, value)).isFile()
+}
 
 const createLink = (pathDir, value) => {
-  if (!fs.lstatSync(path.join(pathDir, value)).isFile()) {
+  if (!isFile(pathDir, value)) {
     return `<a href=${'http://localhost:3000/?type=dir&name=' + value}>[Dir] ${value}</a><br>`
   } else {
     return `<a href=${'http://localhost:3000/?type=file&name=' + value}>[File] ${value}</a><br>`
@@ -19,7 +22,6 @@ const server = http.createServer((req, res) => {
 
   if (type === 'file') {
     const readStream = fs.createReadStream(path.join(currentDirection, name), { encoding: 'utf8' })
-
     readStream.on('data', chunk => res.write(chunk))
     readStream.on('end', () => res.end())
   } else {
@@ -45,104 +47,3 @@ const server = http.createServer((req, res) => {
 })
 
 server.listen(3000, 'localhost')
-
-/*
-
-const fs = require('fs')
-const inquirer = require('inquirer')
-const path = require('path')
-
-const findAndWriteStr = (str, pathUser, file) => {
-  const writeFilePath = `${path.join(pathUser, str)}_requests.log`
-  const writeStream = fs.createWriteStream(writeFilePath, {
-    flags: 'a',
-    encoding: 'utf-8'
-  })
-
-  const userFilePath = path.join(pathUser, file)
-  const readStream = fs.createReadStream(userFilePath, 'UTF-8')
-
-  let count = 0
-
-  readStream.on('data', (chunk) => {
-    chunk.split('\n')
-      .forEach((value) => {
-        if (value.includes(str)) {
-          count++
-          writeStream.write(value + '\n')
-        }
-      })
-  })
-
-  readStream.on('end', () => {
-    console.log('Конец чтения файла!')
-    console.log(`Найдено строк: ${count}`)
-
-    if (count) {
-      writeStream.end(() => console.log(`Конец записи файла! ${writeFilePath}`))
-    } else if (!count) {
-      fs.unlink(writeFilePath, err => err ? console.log(err) : console.log('Конец работы программы!.'))
-    }
-  })
-
-  readStream.on('error', (err) => console.log(err))
-}
-
-const questionStr = (path, file) => {
-  console.clear()
-
-  inquirer
-    .prompt([{
-      name: 'str',
-      type: 'input',
-      message: 'Find str in file: '
-    }])
-    .then(answer => answer.str)
-    .then(str => {
-      findAndWriteStr(str, path, file)
-    })
-    .catch(error => console.log(error))
-}
-
-const questionPath = (answer) => {
-  const lastPath = answer
-  const dirLink = fs.readdirSync(lastPath).reverse()
-  const up = '(...)'
-  dirLink.unshift(up)
-
-  console.clear()
-
-  inquirer
-    .prompt([{
-      name: 'fileName',
-      type: 'list',
-      message: `Choose path or file... \n\nCurrent direction: ${lastPath}`,
-      choices: dirLink
-    }])
-    .then(answer => answer.fileName)
-    .then(answer => {
-      if (answer === up) {
-        questionPath(lastPath.substring(0, lastPath.lastIndexOf('\\')))
-      } else {
-        const valuePath = path.join(lastPath, answer)
-
-        if (!fs.lstatSync(valuePath).isFile()) {
-          questionPath(valuePath)
-        } else {
-          questionStr(lastPath, answer)
-        }
-      }
-    })
-    .catch(error => console.log(error.path))
-}
-
-inquirer
-  .prompt([{
-    name: 'path',
-    type: 'input',
-    message: 'Enter folder path: '
-  }])
-  .then(answer => answer.path)
-  .catch(error => console.log(error))
-  .then(path => questionPath(path))
-*/
